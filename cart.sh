86eda0959a38ad374337dd7f174ad2703dd59d16
eadesany <elijah.adesanya@oulu.fi>
2022-11-19 20:34:29 +0000

    cart script

--- cart.sh
+++ cart.sh
@@ -11,9 +11,9 @@ mv cart-main cart
 cd cart
 npm install
 
-curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"
-cd /home/roboshop
-unzip /tmp/cart.zip
-mv cart-main cart
-cd cart
-npm install
+sed -i -e 's/REDIS_ENDPOINT/redis.devopsb69.online/' -e 's/CATALOGUE_ENDPOINT/catalogue.devopsb69.online/' /home/roboshop/cart/systemd.service
+
+mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service
+systemctl daemon-reload
+systemctl restart cart
+systemctl enable cart
