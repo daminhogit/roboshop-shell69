0fad3bd978e1b4856d4a4d773513a33d5054fad0
eadesany <elijah.adesanya@oulu.fi>
2022-11-19 20:25:24 +0000

    redis script

---
+++ cart.sh
@@ -0,0 +1,19 @@
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+yum install nodejs -y
+
+useradd roboshop
+
+curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"
+cd /home/roboshop
+rm -rf cart
+unzip -o /tmp/cart.zip
+mv cart-main cart
+cd cart
+npm install
+
+curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"
+cd /home/roboshop
+unzip /tmp/cart.zip
+mv cart-main cart
+cd cart
+npm install
