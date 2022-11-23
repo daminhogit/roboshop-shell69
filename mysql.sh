19e983a0101129467420b95ef6e3a4ff4961cfa9
eadesany <elijah.adesanya@oulu.fi>
2022-11-23 18:00:13 +0000

    mySQL script

--- mysql.sh
+++ mysql.sh
@@ -1,47 +1,31 @@
+STATUS() {
+  if [ $1 -eq 0 ]; then
+    echo SUCCESS
+  else
+    echo FAILURE
+    exit
+  fi
+}
+
 echo -e "\e[33mDownloading MySQL Repo File\e[0m"
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-  echo FAILURE
-  exit
-fi
+STAT $?
 
 echo Disable MySQL 8 Version repo
 dnf module disable mysql -y
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-  echo FAILURE
-  exit
-fi
+STAT $?
 
 echo install MySQL
 yum install mysql-community-server -y
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-  echo FAILURE
-  exit
-fi
+STAT $?
 
 echo Enable MySQL Service
 systemctl enable mysqld
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-  echo FAILURE
-  exit
-fi
+STAT $?
 
 echo Start MySQL Service
 systemctl restart mysqld
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-  echo FAILURE
-  exit
-fi
+STAT $?
 
 echo show databases | mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD}
 if [ $? -ne 0 ]
