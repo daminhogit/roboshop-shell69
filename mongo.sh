5f71ea8e320b8ea2e2a8bb9767cff9927d5d823d
eadesany <elijah.adesanya@oulu.fi>
2022-11-24 21:05:25 +0000

    Cart script

--- mongo.sh
+++ mongo.sh
@@ -3,32 +3,32 @@ PRINT ""
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>$LOG
 STAT $?
 
-PRINT ""
+PRINT "Install MongoDB"
 yum install -y mongodb-org &>>$LOG
 STAT $?
 
-PRINT ""
+PRINT "Configure MongoDB"
 sed -i -e 's/127.0.0.1/0.0.0.0/'  /etc/mongod.conf &>>$LOG
 STAT $?
 
 
-PRINT ""
+PRINT "Enable MongoDB"
 systemctl enable mongod &>>$LOG
 STAT $?
 
-PRINT""
+PRINT"Restart MongoDB Service"
 systemctl restart mongod &>>$LOG
 STAT $?
 
 
-PRINT ""
+PRINT "Download MongoDB File"
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>$LOG
 STAT $?
 
 cd /tmp
 
 
-PRINT ""
+PRINT "Extracting MongoDB File"
 unzip -o mongodb.zip &>>$LOG
 STAT $?
 
