1142835f6ee97ebd0e0a5c223636e9ee886a76d8
eadesany <elijah.adesanya@oulu.fi>
2022-11-24 19:03:05 +0000

    Cart script

--- frontend.sh
+++ frontend.sh
@@ -9,26 +9,26 @@ STAT $?
 PRINT "Download Frontend App"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG
 cd /usr/share/nginx/html &>>$LOG
-STAT$?
+STAT $?
 
 rm -rf *
 
 PRINT "Extract App Content"
 unzip /tmp/frontend.zip &>>$LOG
-STAT$?
+STAT $?
 
 mv frontend-main/static/* . &>>$LOG
-STAT$?
+STAT $?
 
 PRINT "Move Config Files"
 mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG
-STAT$?
+STAT $?
 
 PRINT "Enable Nginx"
 systemctl enable nginx &>>$LOG
-STAT$?
+STAT $?
 
 PRINT "Restart Nginx"
 systemctl restart nginx &>>$LOG
-STAT$?
+STAT $?
 
--- mongo.sh
+++ mongo.sh
@@ -1,47 +1,47 @@
 source common.sh
 PRINT ""
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>$LOG
-STAT$?
+STAT $?
 
 PRINT ""
 yum install -y mongodb-org &>>$LOG
-STAT$?
+STAT $?
 
 PRINT ""
 sed -i -e 's/127.0.0.1/0.0.0.0/'  /etc/mongod.conf &>>$LOG
-STAT$?
+STAT $?
 
 
 PRINT ""
 systemctl enable mongod &>>$LOG
-STAT$?
+STAT $?
 
 PRINT""
 systemctl restart mongod &>>$LOG
-STAT$?
+STAT $?
 
 
 PRINT ""
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>$LOG
-STAT$?
+STAT $?
 
 cd /tmp
 
 
 PRINT ""
 unzip -o mongodb.zip &>>$LOG
-STAT$?
+STAT $?
 
 
 cd mongodb-main
 
 PRINT ""
 mongo < catalogue.js &>>$LOG
-STAT$?
+STAT $?
 
 PRINT ""
 mongo < users.js &>>$LOG
-STAT$?
+STAT $?
 
 
 
