41a07a8827602225da4321b28d75c7994eeb790c
eadesany <elijah.adesanya@oulu.fi>
2022-11-24 18:56:12 +0000

    Cart script

--- mongo.sh
+++ mongo.sh
@@ -1,17 +1,48 @@
-curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo
+COMPONENT=mongodb
+source common.sh
+PRINT ""
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>$LOG
+STAT$?
 
-yum install -y mongodb-org
-sed -i -e 's/127.0.0.1/0.0.0.0/'  /etc/mongod.conf
+PRINT ""
+yum install -y mongodb-org &>>$LOG
+STAT$?
 
-systemctl enable mongod
-systemctl restart mongod
+PRINT ""
+sed -i -e 's/127.0.0.1/0.0.0.0/'  /etc/mongod.conf &>>$LOG
+STAT$?
 
-curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
+
+PRINT ""
+systemctl enable mongod &>>$LOG
+STAT$?
+
+PRINT""
+systemctl restart mongod &>>$LOG
+STAT$?
+
+
+PRINT ""
+curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>$LOG
+STAT$?
 
 cd /tmp
-unzip -o mongodb.zip
+
+
+PRINT ""
+unzip -o mongodb.zip &>>$LOG
+STAT$?
+
+
 cd mongodb-main
-mongo < catalogue.js
-mongo < users.js
+
+PRINT ""
+mongo < catalogue.js &>>$LOG
+STAT$?
+
+PRINT ""
+mongo < users.js &>>$LOG
+STAT$?
+
 
 
